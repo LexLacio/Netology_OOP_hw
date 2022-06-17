a9ab0537a1a97f210c14a8607462575416fedb6e
Александр Лацинов <a8880@mail.ru>
2022-06-17 10:21:34 +0300

    New print forms + average grades were added

--- oop_hw.py
+++ oop_hw.py
@@ -16,14 +16,57 @@ class Student:
         else:
             return 'Ошибка'
 
+    def stud_av_grade(self):
+        stud_average_grade = 0
+        for course, grade in self.grades.items():
+            for mark in grade:
+                stud_average_grade += mark
+        result = stud_average_grade / len(grade)
+        return result 
+    
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Не могу сравнивать с не студентами!')
+            return
+        return self.stud_av_grade() < other.stud_av_grade()    
+
+    def __str__(self):
+        res = (f'Имя: {self.name} \nФамилия: {self.surname}\n'
+        f'Курсы в процессе изучения: {" ".join(best_student.courses_in_progress)}\n'
+        f'Средняя оценка за домашние задания: {self.stud_av_grade()} \nЗавершенные курсы: {" ".join(self.finished_courses)}')
+        return res
+
+    
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+        
 class Lecturer(Mentor):
-    lecturer_grades = {}
+    def __init__(self, name, surname):
+        super().__init__(name, surname) 
+        self.lecturer_grades = {}   
+
+    def av_grade(self):
+        average_grade = 0
+        for course, grades in self.lecturer_grades.items():
+            for mark in grades:
+                average_grade += mark
+        result = average_grade / len(grades)
+        return result
+    
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Не могу сравнивать с не лекторами!')
+            return
+        return self.av_grade() < other.av_grade()
+
+    def __str__(self):
+        res = f'Имя: {self.name} \nФамилия: {self.surname}\nСредняя оценка за лекции: {self.av_grade()}'
+        return res
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -34,6 +77,11 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        res = f'Имя: {self.name} \nФамилия: {self.surname}'
+        return res
+
 
 Oleg = Lecturer("Олег", "Булыгин")
 Oleg.courses_attached += ['Python', 'Java']
